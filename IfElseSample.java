d41e7f4b4e83d4c446788dbb397a18f4140a71a4
akshataShirsath21 <akshatashirsath21@gmail.com>
2026-01-04 23:17:39 +0530

    Add files via upload
    DAY 2 Today i tried to solve basic if-else problem a bit different way and it way took my time to understand the
     REAL CONCEPT OF IF-ELSE like
     (condition)=true
     i.e that perticular block of code going to be execute ....
    BUT WHEN THE CONDITION ITSELF IS TRUE THEN............
    (TRUE)=true then...this is what I do for day2....stay tuned!!!

---
+++ IfElseSample.java
@@ -0,0 +1,18 @@
+package Java_Practice;
+
+public class IfElseSample {
+    public static void main(String[] args) {
+
+        boolean isSelected = false;
+
+        if (isSelected){
+            System.out.println("BETTER LUCK NEXT TIME!! ");
+        } else if (true)
+            {
+                System.out.println("YAY ! YOU ARE SELECTED");
+            }else {
+            System.out.println("ERROR404");
+        }
+        }
+    }
+
